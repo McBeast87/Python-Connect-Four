944f14fc929d2f6d7d3f96c8d2e8b2ddf333fa5f
Trevor Cassell <tjcassell87@gmail.com>
2022-07-13 00:33:10 -0400

    initial board setup with examples

---
+++ board setup.py
@@ -0,0 +1,7 @@
+board = [[' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ']]
+
+#Python is zero-indexed so [0] below would represent the first column and also why [6] represents the 7th and final column on the board
+board [0][4] = 'X'
+board [6][2] = 'O'
+print(board)
